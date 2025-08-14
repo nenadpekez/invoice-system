eafa8c044de81fa7203c9ab535c4edd48a5425fa
Nenad Pekez <nenad.pekez@gmail.com>
2025-08-14 12:38:46 +0200

    init

--- application/config/database.php
+++ application/config/database.php
@@ -83,7 +83,7 @@ $db['default'] = array(
 	//'password' 		=> 'vKqP]c&m#4(.',
 	'database' 		=> 'nenadpek_pis',
 	'dbdriver' 		=> 'mysqli',
-	'dbprefix' 		=> '_idr_',
+	'dbprefix' 		=> 'idr_',
 	'pconnect' 		=> FALSE,
 	'db_debug' 		=> (ENVIRONMENT !== 'production'),
 	'cache_on' 		=> FALSE,
